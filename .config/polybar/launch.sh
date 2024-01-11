d4bd79facab11c1b65b47b172407f9c3181784b2
madnessday666 <madnessday666@local.Dlink>
2024-01-11 22:48:08 +0300

    #13 update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,16 @@ pkill polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bars
+sleep 0.1
+
 polybar -q TopLeft -r &
-polybar -q TopRight -r &
+
+sleep 0.1
+
 polybar -q TopCenter -r &
+
+sleep 0.1
+
+polybar -q TopRight -r &
+
+sleep 0.1
--- setup.sh
+++ setup.sh
@@ -40,15 +40,73 @@ check_env() {
 
 	echo $e":"$home/.local/bin >> $home/.bashrc
 
-	echo $e":"$home/.local/bin >> $home/.profile
-
-	echo $e":"$home/.local/bin >> $home/.zshrc
-
 	read input
 
 	pid="$(who -u | awk '{print $6}')"
 
 	kill $pid
+
+}
+
+create_dirs_and_move_files() {
+
+	cd $home
+
+	nonroot 'mkdir Downloads'
+
+	nonroot 'mkdir Screenshots'
+
+	nonroot "mkdir -p $home/.local/share/fonts"
+
+	cd $dir
+
+	nonroot "cp -r fonts/. $home/.local/share/fonts"
+
+	nonroot "cp -r settings/. $home"
+
+	nonroot "cp -r .config $home"
+
+}
+
+service_setup() {
+
+	ln -s /etc/sv/containerd /var/service/
+
+	ln -s /etc/sv/dbus /var/service/
+
+	ln -s /etc/sv/docker /var/service/
+
+	ln -s /etc/sv/elogind /var/service/
+
+	ln -s /etc/sv/NetworkManager /var/service/
+
+	ln -s /etc/sv/polkitd /var/service/
+
+	cd /var/service
+
+	rm -rf acpid
+
+	rm -rf wpa_supplicant
+
+	rm -rf dhcpcd*
+
+	groupadd power
+
+	usermod -aG power $user
+
+	echo '\npolkit.addRule(function(action, subject) {
+	if ((action.id == "org.freedesktop.login1.reboot" ||
+          	action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+          	action.id == "org.freedesktop.login1.power-off" ||
+          	action.id == "org.freedesktop.login1.power-off-multiple-sessions" ||
+          	action.id == "org.freedesktop.login1.suspend" ||
+          	action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
+          	action.id == "org.freedesktop.login1.hibernate" ||
+          	action.id == "org.freedesktop.login1.hibernate-multiple-sessions") && subject.isInGroup("power"))
+     	{
+     	return polkit.Result.YES;
+     	}
+ 	})' | tee -a /etc/polkit-1/rules.d/*.rules
 
 }
 
@@ -113,14 +171,27 @@ install_packages() {
 
 	echo "\nStart installation...\n"
 
+	create_dirs_and_move_files
+
 	xbps-install -y \
-	alacritty bspwm curl dbus dbus-devel dbus-libs dbus-x11 docker dunst elogind feh firefox \
-	flameshot gcc htop leafpad libconfig libconfig-devel libconfig++ libconfig++-devel libev \
-	libev-devel libevdev libglvnd libglvnd-devel libX11 libX11-devel libxcb libxcb-devel \
-	libxdg-basedir nano neofetch NetworkManager numlockx pavucontrol pcre2 pcre2-devel pixman \
-	pixman-devel polkit polybar pulseaudio python3-pipx python3-pkgconfig ranger rofi sxhkd unzip \
-	uthash xcb-util-image xcb-util-image-devel xcb-util-renderutil xcb-util-renderutil-devel \
-	xorg xscreensaver zsh
+	alacritty bspwm curl dbus dbus-devel dbus-libs dbus-x11 docker dunst elogind feh firefox flameshot \
+	gcc htop leafpad libconfig libconfig-devel libconfig++ libconfig++-devel libev libev-devel libevdev \
+	libglvnd libglvnd-devel libX11 libX11-devel libxcb libxcb-devel libxdg-basedir make nano neofetch \
+	NetworkManager numlockx pavucontrol pcre2 pcre2-devel pixman pixman-devel polkit polybar pulseaudio \
+	python3-pipx python3-pkgconfig ranger rofi sxhkd unzip uthash xcb-util-image xcb-util-image-devel \
+	xcb-util-renderutil xcb-util-renderutil-devel xorg xscreensaver zsh
+
+	runuser -u $user -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
+	runuser -u $user -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+	cd .oh-my-zsh/custom && nonroot 'git clone https://github.com/spaceship-prompt/spaceship-prompt.git'
+
+	ln -s $home/.oh-my-zsh/custom/spaceship-prompt/spaceship.zsh-theme $home/.oh-my-zsh/themes/spaceship.zsh-theme
+
+	echo "default_linemode devicons" >> $home/.config/ranger/rc.conf
+
+	nonroot "git clone https://github.com/alexanderjeurissen/ranger_devicons $home/.config/ranger/plugins/ranger_devicons"
 
 	nonroot 'pipx install meson'
 
@@ -128,47 +199,17 @@ install_packages() {
 
 	nonroot 'pipx ensurepath'
 
-	cd $home && nonroot 'mkdir Downloads'
-
-	cd Downloads && nonroot 'git clone https://github.com/allusive-dev/compfy.git' && cd compfy
+	cd $home/Downloads && nonroot 'git clone https://github.com/allusive-dev/compfy.git' && cd compfy
 
 	meson setup . build && ninja -C build && ninja -C build install
 
 	cd $home/Downloads && rm -r -f compfy
 
-	cd $dir && tar -cf -  --exclude '.git' --exclude 'setup.sh' . | tar -xC $home
-
 	cd $home/.config/polybar && chmod +x launch.sh
 
-	nonroot 'git clone https://github.com/alexanderjeurissen/ranger_devicons '$home/.config/ranger/plugins/ranger_devicons
-
-	echo "default_linemode devicons" >> $home/.config/ranger/rc.conf
-
-	ln -s /etc/sv/containerd /var/service/
-
-	ln -s /etc/sv/dbus /var/service/
-
-	ln -s /etc/sv/docker /var/service/
-
-	ln -s /etc/sv/elogind /var/service/
-
-	ln -s /etc/sv/NetworkManager /var/service/
-
-	ln -s /etc/sv/polkitd /var/service/
-
-	cd /var/service
-
-	rm -rf acpid
-
-	rm -rf wpa_supplicant
-
-	rm -rf dhcpcd*
-
-	runuser -u $user -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
-
-	runuser -u $user -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
 	chsh -s /bin/zsh $user
+
+	service_setup
 
 	delimiter
 
@@ -194,4 +235,8 @@ init() {
 
 }
 
-init
+create_dirs_and_move_files
+
+exit
+
+#init
