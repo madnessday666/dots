4aad494c6d8cfb12cf319fbcd7ad26f486d0dc09
madnessday666 <madnessday666@local.Dlink>
2024-02-22 13:42:36 +0300

    update: added JDK to installable packages. Code refactoring "setup.sh"

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 user="$(logname)"
-home=/home/$user
+home="/home/$user"
 dir="$(dirname "$(readlink -f "$0")")"
 
 check_condition() {
@@ -19,36 +19,35 @@ check_condition() {
 
 check_env() {
 	e="$(sudo -Hiu $user env | grep ^PATH)"
-
 	#If user have PATH variable proceed with the installation
 	case $e in
-		*$home/.local/bin*)
-		install_packages;
-		exit;;
+		*$home/.local/bin* && *$home/.jdks/default/bin*)
+		echo "PATH variable is OK";
+		;;
+		*)
+		echo "
+			\r====================================================
+			\nPATH variable needs to be updated to continue \
+			\n(this will be done automatically), \
+			\nyou will need to log in and rerun the script.
+			\n[Press Enter to continue]
+			\n====================================================\n"
+			#Set PATH variable
+			read input
+				if [ ! -f $home/.bashrc ]; then
+					nr "echo -e \
+					#!/bin/bash\n\nexport $e:$home/.local/bin:$home/.jdks/default/bin" \
+					tee -a $home/.bashrc >/dev/null
+						else
+					nr "echo -e \
+					\nexport $e:$home/.local/bin:$home/.jdks/default/bin" \
+					| tee -a $home/.bashrc >/dev/null
+				fi
+			#Logout
+			pid="$(who -u | awk '{print $6}')"
+			kill $pid
+			;;
 	esac
-
-	echo "
-	\r====================================================
-	\nPATH variable must be set to continue,
-	\nyou must log in again and run the script 
-	\n[Press Enter to continue]
-	\n====================================================\n"
-
-	#If the user does not have a PATH variable, it will be set
-	read input
-	if [ ! -f $home/.bashrc ]; then
-		nr "echo -e \
-		#!/bin/bash\n\nexport $e:$home/.local/bin" \
-		tee -a $home/.bashrc >/dev/null
-    	else
-		nr "echo -e \
-		\nexport $e:$home/.local/bin" \
-		| tee -a $home/.bashrc >/dev/null
-	fi
-	
-	#Logout
-	pid="$(who -u | awk '{print $6}')"
-	kill $pid
 }
 
 check_for_updates() {
@@ -61,6 +60,15 @@ check_for_updates() {
 	\rSystem files updated!
 	\n====================================================\n"
 	sleep 2
+}
+
+clean_up() {
+	rm $home/Downloads/SourceCodePro.tar.xz
+	rm $home/Downloads/cursor.tar.gz
+	rm -r -f $home/Downloads/compfy
+	rm -r -f $home/Downloads/lite-xl
+	rm -r -f $home/Downloads/scientifica
+	xbps-remove -R fontforge
 }
 
 copy_user_files() {
@@ -77,6 +85,7 @@ create_dirs() {
 	nr "mkdir Screenshots"
 	nr "mkdir Screenrecs"
 	nr "mkdir .icons"
+	nr "mkdir .jdks"
 	nr "mkdir -p $home/.config/lite-xl/plugins"
 	nr "mkdir -p $home/.local/share/fonts"
 }
@@ -105,59 +114,68 @@ echo "
                                  ╚████╔╝      ██║██╗╚██████╔╝                                         
                                   ╚═══╝       ╚═╝╚═╝ ╚═════╝                                          
                                                                                               "
-	sleep 2
+	sleep 1
 	echo "Do you want to continue? [y/N]:"
-	check_condition check_env quit_installation
+	check_condition start_installation quit_installation
 }
 
-install_packages() {
-	check_for_updates
-	echo "Start installation...\n"
-	sleep 2
-
-	#Install main packages
+install_main_packages() {
 	xbps-install -y \
 	alacritty alsa-plugins-pulseaudio bspwm chrony curl dbus dbus-devel dbus-libs dbus-x11 \
 	docker docker-compose dunst elogind exa feh ffmpeg firefox flameshot font-awesome6 \
-	gcc htop libconfig libconfig-devel libconfig++ libconfig++-devel libev libev-devel \
-	libevdev libglvnd libglvnd-devel libX11 libX11-devel libxcb libxcb-devel libxdg-basedir \
-	lightdm lightdm-gtk3-greeter lite-xl make micro mpv neofetch NetworkManager numlockx \
-	pavucontrol pcre2 pcre2-devel pixman pixman-devel polkit polybar pulseaudio python3-pipx \
-	python3-pkgconfig ranger rofi slop sxhkd unzip uthash xcb-util-image xcb-util-image-devel \
-	xcb-util-renderutil xcb-util-renderutil-devel xdg-utils xdotool xorg xscreensaver zsh
+	fontforge gcc htop libconfig libconfig-devel libconfig++ libconfig++-devel libev \
+	libev-devel libevdev libglvnd libglvnd-devel libX11 libX11-devel libxcb libxcb-devel \
+	libxdg-basedir lightdm lightdm-gtk3-greeter lite-xl make micro mpv neofetch NetworkManager \
+	numlockx pavucontrol pcre2 pcre2-devel pixman pixman-devel polkit polybar pulseaudio \
+	python3-pipx python3-pkgconfig ranger rofi slop sxhkd unzip uthash xcb-util-image \
+	xcb-util-image-devel xcb-util-renderutil xcb-util-renderutil-devel xdg-utils xdotool \
+	xorg xscreensaver zsh
+}
 
+start_installation() {
+	echo "Start installation...\n"
+	sleep 1
+
+	check_env
+	check_for_updates
 	create_dirs
+	install_main_packages
 	install_external_packages
 	copy_user_files
 	manage_service
-
-	echo "Installation completed, reboot now? [y/N]:"
+	clean_up
 	
+	echo "Installation completed, reboot now? [y/N]:"
 	check_condition reboot_system quit_installation  
 }
 
 install_external_packages() {
-	#Install meson and ninja
-	nr "pipx install meson"
-	nr "pipx install ninja"
-	nr "pipx ensurepath"
-
 	#Install JetBrainsMono font
 	nr "curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh" | nr bash
 	
 	#Install ohmyzsh
 	nr "curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh" | nr bash --unattended
 
-	nr "git clone https://github.com/allusive-dev/compfy.git $home/Downloads/compfy"
-	nr "git clone https://github.com/lite-xl/lite-xl-plugins.git $home/Downloads/lite-xl"
+	#Install ranger devicons
 	nr "git clone https://github.com/alexanderjeurissen/ranger_devicons $home/.config/ranger/plugins/ranger_devicons"
 
-	#Install scientifica font
+	#Install JDK
 	nr "curl -L \
-		https://github.com/nerdypepper/scientifica/releases/latest/download/scientifica.tar \
-		--output $home/Downloads/scientifica.tar"
-	nr "tar xfv $home/Downloads/scientifica.tar -C $home/Downloads"
-	nr "cp $home/Downloads/scientifica/ttf/scientifica.ttf $home/.local/share/fonts"
+	"$(curl -fsSL https://api.github.com/repos/adoptium/temurin17-binaries/releases/latest \
+	| grep -Eo "https.*OpenJDK17U-jdk.*x64_linux.*\.tar\.gz" \
+	| head -n1)" \
+	--output $home/Downloads/jdk17.tar.gz"
+	nr "tar xfv $home/Downloads/jdk17.tar.gz -C $home/.jdks"
+	nr "ln -s $home/.jdks/jdk* $home/.jdks/default"
+
+	#Install scientifica font
+	nr "git clone https://github.com/Computer-M/scientifica.git $home/Downloads/scientifica"
+	nr "curl -o BitsNPicas.jar \
+	https://github.com/kreativekorp/bitsnpicas/blob/master/downloads/BitsNPicas.jar \
+	--output-dir $home/Downloads/scientifica"
+	cd $home/Downloads/scientifica && nr "bash $home/Downloads/scientifica/build.sh"
+	nr "cp $home/Downloads/scientifica/build/scientifica/otb/scientifica.otb \
+	$home/.local/share/fonts"
 	
 	#Install SourceCodePro (Nerd patched) font
 	nr "curl -L \
@@ -173,13 +191,20 @@ install_external_packages() {
 	--output $home/Downloads/cursor.tar.gz"
 	nr "tar xfv $home/Downloads/cursor.tar.gz -C $home/.icons"
 
-	#Build and install compfy
+	#Install meson and ninja
+	nr "pipx install meson"
+	nr "pipx install ninja"
+	nr "pipx ensurepath"
+
+	#Install compfy
+	nr "git clone https://github.com/allusive-dev/compfy.git $home/Downloads/compfy"
 	cd $home/Downloads/compfy \
 	&& meson setup . build \
 	&& ninja -C build \
 	&& ninja -C build install
 
-	#Install plugins for lite-xl
+	#Install lite-xl and plugins
+	nr "git clone https://github.com/lite-xl/lite-xl-plugins.git $home/Downloads/lite-xl"
 	cd $home/Downloads/lite-xl/plugins \
 	&& nr "cp \
 	align_carets.lua \
@@ -190,14 +215,6 @@ install_external_packages() {
 	selectionhighlight.lua \
 	language_*.lua \
 	$home/.config/lite-xl/plugins"
-
-	#Clean up
-	rm $home/Downloads/scientifica.tar
-	rm $home/Downloads/SourceCodePro.tar.xz
-	rm $home/Downloads/cursor.tar.gz
-	rm -r -f $home/Downloads/compfy
-	rm -r -f $home/Downloads/lite-xl
-	rm -r -f $home/Downloads/scientifica
 }
 
 manage_service() {
@@ -240,11 +257,11 @@ manage_service() {
  	})' | tee -a /etc/polkit-1/rules.d/*.rules >/dev/null
 
 	#Change shell to zsh
- 	chsh -s /bin/zsh $user
+	chsh -s /bin/zsh $user
 
- 	echo "
- 	\rService managing completed!
- 	\n====================================================\n"
+	echo "
+	\rService managing completed!
+	\n====================================================\n"
  	
 	sleep 2
 }
@@ -271,8 +288,6 @@ quit_installation() {
 	exit
 }
 
-#init
-
-
+check_env
 
 exit
