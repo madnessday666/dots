48673e178e60326a087c8e48e4ad935d399f4354
madnessday666 <sanyatentacion@gmail.com>
2024-02-24 20:50:29 +0300

    update: advanced plug-in installation for lite-xl

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ check_env() {
 	e="$(sudo -Hiu $user env | grep ^PATH)"
 	#If user have PATH variable proceed with the installation
 	case $e in
-		*$home/.local/bin* && *$home/.jdks/default/bin*)
+		*$home/.local/bin*|*$home/.jdks/default/bin*)
 		echo "PATH variable is OK";
 		;;
 		*)
@@ -68,7 +68,9 @@ clean_up() {
 	rm $home/Downloads/jdk17.tar.gz
 	rm $home/Downloads/SourceCodePro.tar.xz
 	rm -r -f $home/Downloads/compfy
-	rm -r -f $home/Downloads/lite-xl
+	rm -r -f $home/Downloads/lite-xl-plugins
+	rm -r -f $home/Downloads/lite-xl-colors
+	rm -r -f $home/Downloads/lite-xl-terminal
 	rm -r -f $home/Downloads/scientifica
 }
 
@@ -85,10 +87,9 @@ create_dirs() {
 	nr "mkdir Downloads"
 	nr "mkdir Screenshots"
 	nr "mkdir Screenrecs"
+	nr "mkdir -p .config/lite-xl"
 	nr "mkdir .icons"
 	nr "mkdir .jdks"
-	nr "mkdir -p $home/.config/lite-xl/plugins"
-	nr "mkdir -p $home/.local/share/fonts"
 }
 
 init() {
@@ -99,36 +100,36 @@ echo "
              ╚██╗ ██╔╝██║   ██║██║██║  ██║    ██║     ██║██║╚██╗██║██║   ██║ ██╔██╗                
               ╚████╔╝ ╚██████╔╝██║██████╔╝    ███████╗██║██║ ╚████║╚██████╔╝██╔╝ ██╗               
                ╚═══╝   ╚═════╝ ╚═╝╚═════╝     ╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝               
-                                                                                              
-███████╗███████╗████████╗██╗   ██╗██████╗     ██╗  ██╗███████╗██╗     ██████╗ ███████╗██████╗ 
-██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗    ██║  ██║██╔════╝██║     ██╔══██╗██╔════╝██╔══██╗
-███████╗█████╗     ██║   ██║   ██║██████╔╝    ███████║█████╗  ██║     ██████╔╝█████╗  ██████╔╝
-╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝     ██╔══██║██╔══╝  ██║     ██╔═══╝ ██╔══╝  ██╔══██╗
-███████║███████╗   ██║   ╚██████╔╝██║         ██║  ██║███████╗███████╗██║     ███████╗██║  ██║
-╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝         ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚══════╝╚═╝  ╚═╝
-                                                                                              
-                                ██╗   ██╗     ██╗    ██████╗                                          
-                                ██║   ██║    ███║   ██╔═████╗                                         
-                                ██║   ██║    ╚██║   ██║██╔██║                                         
-                                ╚██╗ ██╔╝     ██║   ████╔╝██║                                         
-                                 ╚████╔╝      ██║██╗╚██████╔╝                                         
-                                  ╚═══╝       ╚═╝╚═╝ ╚═════╝                                          
-                                                                                              "
+                                                                                                   
+███████╗███████╗████████╗██╗   ██╗██████╗     ██╗  ██╗███████╗██╗     ██████╗ ███████╗██████╗      
+██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗    ██║  ██║██╔════╝██║     ██╔══██╗██╔════╝██╔══██╗     
+███████╗█████╗     ██║   ██║   ██║██████╔╝    ███████║█████╗  ██║     ██████╔╝█████╗  ██████╔╝     
+╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝     ██╔══██║██╔══╝  ██║     ██╔═══╝ ██╔══╝  ██╔══██╗     
+███████║███████╗   ██║   ╚██████╔╝██║         ██║  ██║███████╗███████╗██║     ███████╗██║  ██║     
+╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝         ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚══════╝╚═╝  ╚═╝     
+                                                                                                   
+                                ██╗   ██╗     ██╗    ██████╗                                       
+                                ██║   ██║    ███║   ██╔═████╗                                      
+                                ██║   ██║    ╚██║   ██║██╔██║                                      
+                                ╚██╗ ██╔╝     ██║   ████╔╝██║                                      
+                                 ╚████╔╝      ██║██╗╚██████╔╝                                      
+                                  ╚═══╝       ╚═╝╚═╝ ╚═════╝                                       
+                                                                                                   "
 	sleep 1
 	echo "Do you want to continue? [y/N]:"
 	check_condition start_installation quit_installation
 }
 
 install_main_packages() {
-	xbps-install -y \
-	alacritty alsa-plugins-pulseaudio bspwm chrony curl dbus dbus-devel dbus-libs dbus-x11 \
-	docker docker-compose dunst elogind exa feh ffmpeg firefox flameshot font-awesome6 gcc \
-	htop libconfig libconfig-devel libconfig++ libconfig++-devel libev libev-devel libevdev \
-	libglvnd libglvnd-devel libX11 libX11-devel libxcb libxcb-devel libxdg-basedir lightdm \
-	lightdm-gtk3-greeter lite-xl make micro mpv neofetch NetworkManager numlockx pavucontrol \
-	pcre2 pcre2-devel pixman pixman-devel polkit polybar pulseaudio python3-pipx python3-pkgconfig \
-	ranger rofi slop sxhkd unzip uthash xcb-util-image xcb-util-image-devel xcb-util-renderutil \
-	xcb-util-renderutil-devel xdg-utils xdotool xorg xscreensaver zsh
+	xbps-install -Sy \
+	alacritty alsa-plugins-pulseaudio bspwm chrony clipit curl dbus dbus-devel dbus-libs dbus-x11 \
+	docker docker-compose dunst elogind exa feh ffmpeg firefox flameshot font-awesome6 gcc htop \
+	libconfig libconfig-devel libconfig++ libconfig++-devel libev libev-devel libevdev libglvnd \
+	libglvnd-devel libX11 libX11-devel libxcb libxcb-devel libxdg-basedir lightdm lightdm-gtk3-greeter \
+	lite-xl make micro mpv neofetch NetworkManager numlockx pavucontrol pcre2 pcre2-devel pixman \
+	pixman-devel polkit polybar pulseaudio python3-pipx python3-pkgconfig ranger rofi slop sxhkd \
+	unzip uthash xcb-util-image xcb-util-image-devel xcb-util-renderutil xcb-util-renderutil-devel \
+	xdg-utils xdotool xorg xscreensaver zsh
 }
 
 install_external_packages() {
@@ -179,7 +180,7 @@ install_external_packages() {
 	nr "pipx install meson"
 	nr "pipx install ninja"
 	nr "pipx ensurepath"
-
+	
 	#Install compfy
 	nr "git clone https://github.com/allusive-dev/compfy.git $home/Downloads/compfy"
 	cd $home/Downloads/compfy \
@@ -187,18 +188,80 @@ install_external_packages() {
 	&& ninja -C build \
 	&& ninja -C build install
 
-	#Install lite-xl and plugins
-	nr "git clone https://github.com/lite-xl/lite-xl-plugins.git $home/Downloads/lite-xl"
-	cd $home/Downloads/lite-xl/plugins \
+	#Install lite-xl plugins and colorschemes
+	nr "git clone https://github.com/lite-xl/lite-xl-plugins.git $home/Downloads/lite-xl-plugins"
+	nr "git clone https://github.com/lite-xl/lite-xl-colors.git $home/Downloads/lite-xl-colors"
+	nr "git clone https://github.com/adamharrison/lite-xl-terminal.git $home/Downloads/lite-xl-terminal"
+	##Plugins
+	nr "cp -R $home/Downloads/lite-xl-terminal/plugins $home/.config/lite-xl"
+	nr "curl -L \
+	https://github.com/adamharrison/lite-xl-terminal/releases/download/latest/libterminal.x86_64-linux.so \
+	--output $home/.config/lite-xl/plugins/terminal/libterminal.x86_64-linux.so"
+	while read line
+		do
+			case $line in
+				*black* )
+				replacement=$replacement"    \[  0\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				replacement=$replacement"    \[  8\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				;;
+				*blue* )
+				replacement=$replacement"    \[  4\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				replacement=$replacement"    \[ 12\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				;;
+				*cyan* )
+				replacement=$replacement"    \[  6\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				replacement=$replacement"    \[ 14\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				;;
+				*green* )
+				replacement=$replacement"    \[  2\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				replacement=$replacement"    \[ 10\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				;;
+				*magenta* )
+				replacement=$replacement"    \[  5\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				replacement=$replacement"    \[ 13\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				;;
+				*red* )
+				replacement=$replacement"    \[  1\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				replacement=$replacement"    \[  9\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				;;
+				*white* )
+				replacement=$replacement"    \[  7\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				replacement=$replacement"    \[ 15\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				;;
+				*yellow* )
+				replacement=$replacement"    \[  3\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				replacement=$replacement"    \[ 11\] = { common.color $(echo $line | sed -E -e 's/.*(.*\"#.*).*/\1/g') },\n";
+				;;
+				*colors.normal* )
+				break
+				;;
+			esac
+		done < "$dir/.config/alacritty/alacritty.toml"
+	row=$row"s/.*background = { common.color \"#.*\" },.*\s/  background = { common.color \"#404040\" },/;"
+	row=$row"s/.*\[  5\] = { common.color \"#.*\" },.*\s//;"
+	row=$row"s/.*\[ 10\] = { common.color \"#.*\" },.*\s//;"
+	row=$row"s/.*\[ 15\] = { common.color \"#.*\" },.*\s//;"
+	row=$row"s/.*\[  0\] = { common.color \"#.*\" },.*\s/$replacement/"
+	perl -pi -e "$row" $home/.config/lite-xl/plugins/terminal/init.lua
+	cd $home/Downloads/lite-xl-plugins/plugins \
 	&& nr "cp \
 	align_carets.lua \
 	autoinsert.lua \
 	autowrap.lua \
-	ghmarkdown.lua \
+	bracketmatch.lua \
 	colorpreview.lua \
-	selectionhighlight.lua \
+	extend_selection_line.lua \
+	ghmarkdown.lua \
+	indentguide.lua \
 	language_*.lua \
+	restoretabs.lua \
+	selectionhighlight.lua \
+	sort.lua \
+	sticky_scroll.lua \
+	su_save.lua \
 	$home/.config/lite-xl/plugins"
+	##Colorschemes
+	nr "cp -R $home/Downloads/lite-xl-colors/colors $home/.config/lite-xl"
 }
 
 manage_services() {
@@ -220,7 +283,7 @@ manage_services() {
 	#Remove conflicting services
 	cd /var/service
 	rm -rf acpid
-	rm -rf wpa_supplicant
+	rm -rf wpa_supplicantt
 	rm -rf dhcpcd*
 
 	#Add user to group 'power' and create rule for non root power management 
@@ -239,14 +302,13 @@ manage_services() {
      	return polkit.Result.YES;
      	}
  	})' | tee -a /etc/polkit-1/rules.d/*.rules >/dev/null
-
+ 	
 	#Change shell to zsh
-	chsh -s /bin/zsh $user
+	chsh -s /bin/zsh $userr
 
 	echo "
 	\rService managing completed!
 	\n====================================================\n"
- 	
 	sleep 2
 }
 
